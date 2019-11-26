b71e5a47de100675b95d5c3744fa377febd877c1
Mario Pescarmona <mpescarmona@gmail.com>
2019-11-26 15:37:04 -0300

    Initial commit

---
+++ src/main/java/com/mpescarmona/earthquake/earthquake/api/demo/EarthquakeApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mpescarmona.earthquake.earthquake.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeApiDemoApplication.class, args);
+	}
+
+}
