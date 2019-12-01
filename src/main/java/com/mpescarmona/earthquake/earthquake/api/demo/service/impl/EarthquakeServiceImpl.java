6e4f2245abbca71fb22b8ba85625c237195f1415
Mario Pescarmona <mpescarmona@gmail.com>
2019-12-01 15:18:45 -0300

    Add working service implementation to consume USGS EarthQuake API

--- src/main/java/com/mpescarmona/earthquake/earthquake/api/demo/service/impl/EarthquakeServiceImpl.java
+++ src/main/java/com/mpescarmona/earthquake/earthquake/api/demo/service/impl/EarthquakeServiceImpl.java
@@ -1,31 +1,142 @@
 package com.mpescarmona.earthquake.earthquake.api.demo.service.impl;
 
+import com.mpescarmona.earthquake.earthquake.api.demo.domain.Feature;
 import com.mpescarmona.earthquake.earthquake.api.demo.domain.response.EarthquakeResponse;
+import com.mpescarmona.earthquake.earthquake.api.demo.helper.EarthquakeUrlHelper;
 import com.mpescarmona.earthquake.earthquake.api.demo.service.IEarthquakeService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
 public class EarthquakeServiceImpl implements IEarthquakeService {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private EarthquakeUrlHelper earthquakeUrlHelper;
+
     @Override
-    public EarthquakeResponse getEarthquakesByDateRanges(String startTime, String endTime) {
-        return null;
+    public EarthquakeResponse getEarthquakesByDateRange(String startTime, String endTime) {
+        log.info("action=getEarthquakesByDateRange, startTime={}, endTime={}", startTime, endTime);
+        EarthquakeResponse earthquakeResponse = null;
+        try {
+            String url = earthquakeUrlHelper.buildEarthquakeUrlByDates(startTime, endTime);
+            log.info("action=getEarthquakesByDateRange, url={}", url);
+
+            ResponseEntity<EarthquakeResponse> response = callEarthquakeService(url);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                earthquakeResponse = response.getBody();
+            }
+
+            log.info("action=getEarthquakesByDateRange, result={}", response);
+        } catch (Exception ex) {
+            log.error("action=getEarthquakesByDateRange, error={}", ex.getMessage());
+        }
+        return earthquakeResponse;
     }
 
     @Override
-    public EarthquakeResponse getEarthquakesByDateRanges(String startTimeRange1, String endTimeRange1, String startTimeRange2, String endTimeRange2) {
-        return null;
+    public EarthquakeResponse getEarthquakesByDateRanges(String startTimeRange1, String endTimeRange1,
+                                                         String startTimeRange2, String endTimeRange2) {
+        log.info("action=getEarthquakesByDateRanges, startTimeRange1={}, endTimeRange1={}, startTimeRange2={}, " +
+                "endTimeRange2={}", startTimeRange1, endTimeRange1, startTimeRange2, endTimeRange2);
+        EarthquakeResponse responseRangeOne = getEarthquakesByDateRange(startTimeRange1, endTimeRange1);
+        EarthquakeResponse responseRangeTwo = getEarthquakesByDateRange(startTimeRange2, endTimeRange2);
+
+        List<Feature> allFeatures = new ArrayList<>(responseRangeOne.getFeatures());
+        allFeatures.addAll(responseRangeTwo.getFeatures());
+        responseRangeOne.setFeatures(allFeatures);
+        Integer total = responseRangeOne.getMetadata().getCount() + responseRangeTwo.getMetadata().getCount();
+        responseRangeOne.getMetadata().setCount(total);
+
+        log.info("action=getEarthquakesByDateRanges, result={}", responseRangeOne);
+        return responseRangeOne;
     }
 
     @Override
     public EarthquakeResponse getEarthquakesByMagnitudeRanges(String minMagnitude, String maxMagnitude) {
-        return null;
+        log.info("action=getEarthquakesByMagnitudeRanges, minMagnitude={}, maxMagnitude={}", minMagnitude, maxMagnitude);
+        String url = earthquakeUrlHelper.buildEarthquakeUrlByMagnitudes(minMagnitude, maxMagnitude);
+        log.info("action=getEarthquakesByMagnitudeRanges, url={}", url);
+        EarthquakeResponse earthquakeResponse = null;
+        try {
+            ResponseEntity<EarthquakeResponse> response = callEarthquakeService(url);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                earthquakeResponse = response.getBody();
+            }
+
+            log.info("action=getEarthquakesByMagnitudeRanges, result={}", response);
+        } catch (Exception ex) {
+            log.error("action=getEarthquakesByMagnitudeRanges, error={}", ex.getMessage());
+        }
+        return earthquakeResponse;
     }
 
     @Override
     public EarthquakeResponse getEarthquakesByCountry(String country) {
-        return null;
+        log.info("action=getEarthquakesByCountry, country={}", country);
+        String url = earthquakeUrlHelper.buildEarthQuakeBaseUrlAndFormat();
+        log.info("action=getEarthquakesByCountry, url={}", url);
+        EarthquakeResponse earthquakeResponse = null;
+        try {
+            ResponseEntity<EarthquakeResponse> response = callEarthquakeService(url);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                earthquakeResponse = response.getBody();
+            }
+
+            log.info("action=getEarthquakesByCountry, result={}", response);
+        } catch (Exception ex) {
+            log.error("action=getEarthquakesByCountry, error={}", ex.getMessage());
+        }
+        List<Feature> filteredFeatures = earthquakeResponse.getFeatures().stream()
+                .filter(feature -> feature.getProperties().getPlace().contains(country))
+                .collect(Collectors.toList());
+
+        earthquakeResponse.setFeatures(filteredFeatures);
+        earthquakeResponse.getMetadata().setCount(filteredFeatures.size());
+
+        return earthquakeResponse;
     }
 
     @Override
-    public EarthquakeResponse getEarthquakesByCountriesAndDateRanges(String countryONe, String countryTwo, String startTime, String endTime) {
-        return null;
+    public EarthquakeResponse getEarthquakesByCountriesAndDateRange(String countryOne, String countryTwo, String startTime, String endTime) {
+        log.info("action=getEarthquakesByCountriesAndDateRange, countryOne={}, countryTwo={}, startTime={}, endTime={}", countryOne, countryTwo, startTime, endTime);
+        EarthquakeResponse response = getEarthquakesByDateRange(startTime, endTime);
+
+        List<Feature> filteredFeatures = response.getFeatures().stream()
+                .filter(feature -> feature.getProperties().getPlace().contains(countryOne) ||
+                        feature.getProperties().getPlace().contains(countryTwo))
+                .collect(Collectors.toList());
+
+        response.setFeatures(filteredFeatures);
+        response.getMetadata().setCount(filteredFeatures.size());
+
+        log.info("action=getEarthquakesByCountriesAndDateRange, result={}", response);
+        return response;
+    }
+
+    private HttpEntity<String> buildEarthquakeRequestHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        return new HttpEntity<>(headers);
+    }
+
+    private ResponseEntity<EarthquakeResponse> callEarthquakeService(String url) {
+        HttpEntity<String> entity = buildEarthquakeRequestHeader();
+        return restTemplate.exchange(url, HttpMethod.GET, entity, EarthquakeResponse.class);
     }
 }
--- src/test/groovy/com/mpescarmona/earthquake/earthquake/api/demo/service/impl/EarthquakeServiceImplTest.groovy
+++ src/test/groovy/com/mpescarmona/earthquake/earthquake/api/demo/service/impl/EarthquakeServiceImplTest.groovy
@@ -1,26 +1,222 @@
 package com.mpescarmona.earthquake.earthquake.api.demo.service.impl
 
-import spock.lang.Ignore
+import com.mpescarmona.earthquake.earthquake.api.demo.domain.Feature
+import com.mpescarmona.earthquake.earthquake.api.demo.domain.Metadata
+import com.mpescarmona.earthquake.earthquake.api.demo.domain.Properties
+import com.mpescarmona.earthquake.earthquake.api.demo.domain.response.EarthquakeResponse
+import com.mpescarmona.earthquake.earthquake.api.demo.helper.EarthquakeUrlHelper
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
 class EarthquakeServiceImplTest extends Specification {
-    @Ignore
+
+    RestTemplate restTemplate = Mock {}
+    EarthquakeUrlHelper earthquakeUrlHelper = Mock {}
+    EarthquakeServiceImpl earthquakeService = new EarthquakeServiceImpl(restTemplate, earthquakeUrlHelper)
+
     def "GetEarthquakesByDateRanges"() {
+        given:
+        EarthquakeResponse response
+        String startTime = "2019-11-27"
+        String endTime = "2019-11-28"
+        String url = "http://fakeearthquakeservice?format=geoformat&startTime=" + startTime + "&endTime=" + endTime
+
+        when:
+        earthquakeUrlHelper.buildEarthquakeUrlByDates(startTime, endTime) >> url
+
+        List<Properties> properties = Arrays.asList(
+                Properties.builder()
+                        .place("83km W of Copiapo, Chile")
+                        .mag(4.6)
+                        .build()
+        );
+        List<Feature> features = buildFeaturesFromProperties(properties);
+        EarthquakeResponse earthQuakeResponse = buildEarthquakeResponse(features);
+        restTemplate.exchange(url, HttpMethod.GET, _, EarthquakeResponse.class) >> new ResponseEntity(earthQuakeResponse, HttpStatus.OK)
+
+        response = earthquakeService.getEarthquakesByDateRange(startTime, endTime)
+
+        then:
+        response != null
+        response.features.size() == 1
+        response.metadata.count == 1
     }
 
-    @Ignore
-    def "TestGetEarthquakesByDateRanges"() {
-    }
-
-    @Ignore
     def "GetEarthquakesByMagnitudeRanges"() {
+        given:
+        EarthquakeResponse response
+        String minMagnitude = "6.1"
+        String maxMagnitude = "7.5"
+        String url = "http://fakeearthquakeservice?format=geoformat&minMagnitude=" + minMagnitude + "&maxMagnitude=" + maxMagnitude
+
+        when:
+        earthquakeUrlHelper.buildEarthquakeUrlByMagnitudes(minMagnitude, maxMagnitude) >> url
+
+        List<Properties> properties = Arrays.asList(
+                Properties.builder()
+                        .place("83km W of Copiapo, Chile")
+                        .mag(7.4)
+                        .build(),
+                Properties.builder()
+                        .place("62km SE of Molibagu, Indonesia")
+                        .mag(6.1)
+                        .build(),
+                Properties.builder()
+                        .place("24km ESE of Punta de Burica, Panama")
+                        .mag(6.5)
+                        .build()
+        );
+        List<Feature> features = buildFeaturesFromProperties(properties);
+        EarthquakeResponse earthQuakeResponse = buildEarthquakeResponse(features);
+        restTemplate.exchange(url, HttpMethod.GET, _, EarthquakeResponse.class) >> new ResponseEntity(earthQuakeResponse, HttpStatus.OK)
+
+        response = earthquakeService.getEarthquakesByMagnitudeRanges(minMagnitude, maxMagnitude)
+
+        then:
+        response != null
+        response.features.size() == 3
+        response.metadata.count == 3
     }
 
-    @Ignore
+    def "TestGetEarthquakesByDateRanges"() {
+        given:
+        EarthquakeResponse response
+        String startTime1 = "2019-11-20"
+        String endTime1 = "2019-11-21"
+        String url1 = "http://fakeearthquakeservice?format=geoformat&startTime=" + startTime1 + "&endTime=" + endTime1
+        String startTime2 = "2019-11-27"
+        String endTime2 = "2019-11-28"
+        String url2 = "http://fakeearthquakeservice?format=geoformat&startTime=" + startTime2 + "&endTime=" + endTime2
+
+        when:
+        earthquakeUrlHelper.buildEarthquakeUrlByDates(startTime1, endTime1) >> url1
+        earthquakeUrlHelper.buildEarthquakeUrlByDates(startTime2, endTime2) >> url2
+
+        List<Properties> properties1 = Arrays.asList(
+                Properties.builder()
+                        .place("62km SE of Molibagu, Indonesia")
+                        .mag(6.1)
+                        .build(),
+                Properties.builder()
+                        .place("83km W of Copiapo, Chile")
+                        .mag(7.2)
+                        .build()
+        );
+        List<Feature> features1 = buildFeaturesFromProperties(properties1);
+        EarthquakeResponse earthQuakeResponse1 = buildEarthquakeResponse(features1)
+
+        List<Properties> properties2 = Arrays.asList(
+                Properties.builder()
+                        .place("7km N of Zonda, Argentina")
+                        .mag(7.1)
+                        .build()
+        )
+        List<Feature> features2 = buildFeaturesFromProperties(properties2)
+        EarthquakeResponse earthQuakeResponse2 = buildEarthquakeResponse(features2)
+
+        restTemplate.exchange(url1, HttpMethod.GET, _, EarthquakeResponse.class) >> new ResponseEntity(earthQuakeResponse1, HttpStatus.OK)
+        restTemplate.exchange(url2, HttpMethod.GET, _, EarthquakeResponse.class) >> new ResponseEntity(earthQuakeResponse2, HttpStatus.OK)
+
+        response = earthquakeService.getEarthquakesByDateRanges(startTime1, endTime1, startTime2, endTime2)
+
+        then:
+        response != null
+        response.features.size() == 3
+        response.metadata.count == 3
+    }
+
     def "GetEarthquakesByCountry"() {
+        given:
+        EarthquakeResponse response
+        String url = "http://fakeearthquakeservice?format=geoformat";
+
+        when:
+        earthquakeUrlHelper.buildEarthQuakeBaseUrlAndFormat() >> url
+
+        List<Properties> properties = Arrays.asList(
+                Properties.builder()
+                        .place("83km W of Copiapo, Chile")
+                        .mag(7.4)
+                        .build(),
+                Properties.builder()
+                        .place("62km SE of Molibagu, Indonesia")
+                        .mag(6.1)
+                        .build(),
+                Properties.builder()
+                        .place("24km ESE of Punta de Burica, Panama")
+                        .mag(6.5)
+                        .build()
+        );
+        List<Feature> features = buildFeaturesFromProperties(properties);
+        EarthquakeResponse earthQuakeResponse = buildEarthquakeResponse(features);
+        restTemplate.exchange(url, HttpMethod.GET, _, EarthquakeResponse.class) >> new ResponseEntity(earthQuakeResponse, HttpStatus.OK)
+
+        response = earthquakeService.getEarthquakesByCountry("Chile")
+
+        then:
+        response != null
+        response.features.size() == 1
+        response.metadata.count == 1
     }
 
-    @Ignore
     def "GetEarthquakesByCountriesAndDateRanges"() {
+        given:
+        EarthquakeResponse response
+        String startTime = "2019-11-27"
+        String endTime = "2019-11-28"
+        String url = "http://fakeearthquakeservice?format=geoformat&startTime=" + startTime + "&endTime=" + endTime
+
+        when:
+        earthquakeUrlHelper.buildEarthquakeUrlByDates(startTime, endTime) >> url
+
+        List<Properties> properties = Arrays.asList(
+                Properties.builder()
+                        .place("83km W of Copiapo, Chile")
+                        .mag(7.4)
+                        .build(),
+                Properties.builder()
+                        .place("62km SE of Molibagu, Indonesia")
+                        .mag(6.1)
+                        .build(),
+                Properties.builder()
+                        .place("24km ESE of Punta de Burica, Panama")
+                        .mag(6.5)
+                        .build()
+        );
+        List<Feature> features = buildFeaturesFromProperties(properties);
+        EarthquakeResponse earthQuakeResponse = buildEarthquakeResponse(features);
+        restTemplate.exchange(url, HttpMethod.GET, _, EarthquakeResponse.class) >> new ResponseEntity(earthQuakeResponse, HttpStatus.OK)
+
+        response = earthquakeService.getEarthquakesByCountriesAndDateRange("Chile", "Indonesia", startTime, endTime);
+
+        then:
+        response != null
+        response.features.size() == 2
+        response.metadata.count == 2
+    }
+
+    private EarthquakeResponse buildEarthquakeResponse(List<Feature> features) {
+        return EarthquakeResponse.builder()
+                .type("Fake")
+                .metadata(Metadata.builder()
+                        .count(features.size())
+                        .build())
+                .features(features)
+                .build()
+    }
+
+    private List<Feature> buildFeaturesFromProperties(List<Properties> properties) {
+        List<Feature> features = new ArrayList<>();
+        properties.forEach({ property ->
+            features.add(
+                    Feature.builder()
+                            .properties(property)
+                            .build()
+            )
+        })
+        return features;
     }
 }
